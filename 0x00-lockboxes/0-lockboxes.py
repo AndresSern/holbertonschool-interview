41eadbc2c285446c512022abfa7a5f0da38b5c6e
AndresSern <andrecamposer@gmail.com>
2021-07-22 18:31:59 -0500

    Removing spaces and blank lines

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,6 +4,7 @@ You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 """
 
+
 def canUnlockAll(boxes):
     """
         Function that dermines if all the boxes can be opened
@@ -17,4 +18,4 @@ def canUnlockAll(boxes):
             keys = (keys | set(rows))
     if count == 0:
         return len(boxes) == len(boxes)
-    return (len(keys)  == (len(boxes) - count))
+    return (len(keys) == (len(boxes) - count))
