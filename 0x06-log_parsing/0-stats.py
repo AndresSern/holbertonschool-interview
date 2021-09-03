ac4d0d52ed1e064d68eacf2a1de1c6c2a9fca82a
AndresSern <andrecamposer@gmail.com>
2021-09-02 20:58:24 -0500

    Changing the function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,10 +1,52 @@
 #!/usr/bin/python3
-import sys
-import re
 """
 Module that parses a log and prints stats to stdout
 """
+from sys import stdin
 
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+size = 0
+
+
+def print_stats():
+    """Prints the accumulated logs"""
+    print("File size: {}".format(size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
+
+
+if __name__ == "__main__":
+    count = 0
+    try:
+        for line in stdin:
+            try:
+                items = line.split()
+                size += int(items[-1])
+                if items[-2] in status_codes:
+                    status_codes[items[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
+
+'''
 
 def checkInputLog(theInput):
     """ check if the format is the next or else return None
@@ -63,3 +105,4 @@ if __name__ == "__main__":
         print_stats(status_codes, size)
         raise
     print_stats(status_codes, size)
+'''
