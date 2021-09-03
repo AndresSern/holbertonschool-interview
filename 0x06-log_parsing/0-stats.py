a3fcc8aa87dfd1204cc00b8ec7fc8c065e07ddb9
AndresSern <andrecamposer@gmail.com>
2021-09-03 09:22:00 -0500

    Adding the old function

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,51 +2,9 @@
 """
 Module that parses a log and prints stats to stdout
 """
-from sys import stdin
+import sys
+import re
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-size = 0
-
-
-def print_stats():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
-
-
-if __name__ == "__main__":
-    count = 0
-    try:
-        for line in stdin:
-            try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
-            except:
-                pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
-
-'''
 
 def checkInputLog(theInput):
     """ check if the format is the next or else return None
@@ -62,6 +20,13 @@ def checkInputLog(theInput):
     return re.match(pattern, theInput)
 
 
+def print_stats(status_codes, size):
+    """Prints the accumulated logs"""
+    print("File size: {}".format(size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
+
 status_codes = {
     "200": 0,
     "301": 0,
@@ -72,15 +37,6 @@ status_codes = {
     "405": 0,
     "500": 0
 }
-
-
-def print_stats(status_codes, size):
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
-
 
 size = 0
 
@@ -105,4 +61,3 @@ if __name__ == "__main__":
         print_stats(status_codes, size)
         raise
     print_stats(status_codes, size)
-'''
