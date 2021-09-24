a9bfe23575761bcfb85595211c01b6e6cf22de22
AndresSern <andrecamposer@gmail.com>
2021-09-23 23:40:55 -0500

    Changing the function in the file 0-check_cycle.c

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,28 +1,21 @@
 #include "lists.h"
-
 /**
- * check_cycle - checks if a list has a cycle
- * @list: list
- * Return: 0 if there is no cycle, 1 if there is a cycle
- */
+* check_cycle - Check if a singly linked list has a cycle in it
+* @list: pointer to linked list
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle = NULL, *rabbit = NULL;
-	int count = 0;
+	listint_t *turtle = list, *hare = list;
 
-	turtle = rabbit = list;
-
-	while (rabbit != NULL)
+	if (list == NULL)
+		return (0);
+	while (hare != NULL && hare->next != NULL && hare->next->next != NULL)
 	{
-		rabbit = rabbit->next;
-		if (count == 2)
-		{
-			turtle = turtle->next;
-			count = 0;
-		}
-		if (turtle == rabbit)
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
 			return (1);
-		count++;
 	}
 	return (0);
 }
