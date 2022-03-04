84c0965e455035e2a6e3503517996d5245c57706
Andres Campo <68988657+AndresSern@users.noreply.github.com>
2022-03-03 22:44:17 -0500

    Changin

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,36 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "menger.h"
 
+void print_level(int level, int size);
+
 /**
- * menger - draws a 2D Menger Sponge
- * @level: level of the menger sponge to draw
+ * menger - draws a 2D Menger Sponge of given level
+ * @level: level of the Menger Sponge to draw
+ * if level is less than 0, nothing happens
+ * level 0 sponge is represented by #
+ * level N sponge is represented by 3x3 sqaure of level N-1 sponges,
+ * with empty center
  *
  */
+
 void menger(int level)
 {
-	int i, j, result, ii, jj;
-	char simbol;
+	int size = pow(3, level), row = 0, column = 0, pound = 1;
+	int divisor = pow(3, 0), r_remain = 0, c_remain = 0;
 
-	result = pow(3, level);
-
-	for (i = 0; i < result; i++)
+	if (level < 0)
+		return;
+	if (level == 0)
+		printf("#\n");
+	if (level > 0)
 	{
-		for (j = 0; j < result; j++)
+		for (row = 0; row < size; row++)
 		{
-			simbol = '#';
-			ii = i;
-			jj = j;
-			while (ii > 0)
+			for (column = 0; column < size; column++)
 			{
-				if (ii % 3 == 1 && jj % 3 == 1)
-					simbol = ' ';
-				ii /= 3;
-				jj /= 3;
+				pound = 1;
+				for (divisor = 1; divisor < size; divisor *= 3)
+				{
+					r_remain = ((row / divisor) % 3) == 1;
+					c_remain = ((column / divisor) % 3) == 1;
+					if (r_remain && c_remain && pound)
+						pound = 0;
+				}
+				if (pound)
+					printf("#");
+				else
+					printf(" ");
 			}
-			printf("%c", simbol);
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
